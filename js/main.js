e1361d2239f42209e9af73b5b50138b0ae4c9aec
Justin <justin@Justins-MacBook-Pro.local>
2014-09-10 08:30:44 -0600

    updating forms, removing CSS transition, tweaking design layout

--- js/main.js
+++ js/main.js
@@ -6,11 +6,16 @@ $(document).ready(function(){
 	$('.accordion-toggle').on('click', function(e){
 		e.preventDefault();
 		$(this).find('.arrow').toggleClass('down').toggleClass('up');
-		$(this).siblings('.accordion-body').toggleClass('open');
+		// $(this).siblings('.accordion-body').toggleClass('open');
+		$(this).siblings('.accordion-body').slideToggle(250);
+		
 	});
 
 	$('.key').on('click', function(e){
 		e.preventDefault();
 		$(this).closest('section').toggleClass('lock').toggleClass('unlock');
 	});
+
+	// $('.lock').toggleClass('lock').toggleClass('unlock');
+
 });
