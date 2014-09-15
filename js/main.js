1c755ff7dd3d0154614b2fa3dd0f181e75a37db7
Justin Tulk <justintulk@gmail.com>
2014-09-15 09:43:28 -1000

    updating form styling for mobile, adding am/pm slider, adding hours build w/ javascript

--- js/main.js
+++ js/main.js
@@ -1,21 +1,35 @@
-$(document).ready(function(){
-	if($('.select')){
-		$('select').dropkick();
-	}
+$(document).ready(function() {
+    var hourInput = $('.12-hour');
+    console.log(hourInput);
+    $.each(hourInput, function(index, value) {
+        console.log(value);
+        for (i = 1; i < 13; i++) {
+            for (j = 0; j < 4; j++) {
+                var minutes = j * 15;
+                if (minutes == 0) {
+                    minutes = "00";
+                }
+                var time = i + ":" + minutes;
+                console.log(time);
+                $(value).append("<option value='" + time + "'>" + time + "</option>");
+            }
+        }
+    });
 
-	$('.accordion-toggle').on('click', function(e){
-		e.preventDefault();
-		$(this).find('.arrow').toggleClass('down').toggleClass('up');
-		// $(this).siblings('.accordion-body').toggleClass('open');
-		$(this).siblings('.accordion-body').slideToggle(250);
-		
-	});
+    $('.accordion-toggle').on('click', function(e) {
+        e.preventDefault();
+        $(this).find('.arrow').toggleClass('down').toggleClass('up');
+        // $(this).siblings('.accordion-body').toggleClass('open');
+        $(this).siblings('.accordion-body').slideToggle(250);
 
-	$('.padlock').on('click', function(e){
-		e.preventDefault();
-		$(this).closest('section').toggleClass('locked').toggleClass('unlocked');
-	});
+    });
 
-	// $('.lock').toggleClass('lock').toggleClass('unlock');
+    $('.padlock').on('click', function(e) {
+        e.preventDefault();
+        $(this).closest('section').toggleClass('locked').toggleClass('unlocked');
+    });
+
+    $('.datepicker').pikaday();
+    $('select').dropkick();
 
 });
